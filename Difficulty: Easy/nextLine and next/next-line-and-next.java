76a405c50effcbf2cc629a32e0ed43a40c6a3534
sarika12s <sarikachilukamari@gmail.com>
2026-05-02 23:27:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/nextLine and next/next-line-and-next.java
@@ -0,0 +1,17 @@
+// Complete the function
+// Take input for a and s
+class Geeks {
+    static void getInput() {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            sc.nextLine();
+            String s =sc.nextLine() ;
+            System.out.println(a);
+            System.out.println(s);
+        }
+    }
+}
+
