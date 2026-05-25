f03d1d57cb313bd5e3a272ebfc35a45cd2e0d5d7
sarika12s <sarikachilukamari@gmail.com>
2026-05-25 12:15:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print With Space/print-with-space.java
@@ -0,0 +1,8 @@
+
+public class Solution {
+    public void utility(Scanner scn) {
+        String a = scn.nextLine();
+        String b = scn.nextLine();
+        System.out.println(a + " " + b);
+    }
+}
