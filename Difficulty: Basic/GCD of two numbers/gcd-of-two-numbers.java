0c5499e00573ab922ec9b7382a7fea931402d018
sarika12s <sarikachilukamari@gmail.com>
2026-05-28 20:49:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int gcd(int a, int b) {
+        // code here
+        while(a > 0 && b > 0){
+            if(a > b)   a = a - b;
+            else b = b - a;
+        }
+        return a;
+    }
+}
