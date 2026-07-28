42588f91e942bef9f445d8e9436d80abf9391321
sarika12s <sarikachilukamari@gmail.com>
2026-07-28 22:15:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Odd or Even/odd-or-even.java
@@ -0,0 +1,14 @@
+//import java.util.Scanner;
+class Solution {
+    static boolean isEven(int n) {
+        //Scanner sc = new Scanner(System.in);
+        //int n = sc.nextInt();
+        if(n%2==0){
+            return true;
+        }else{
+            return false;
+        }
+        
+        
+    }
+}
