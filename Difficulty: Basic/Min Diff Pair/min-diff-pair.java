73ff9588f8cdb3b63287ddcdbf00a28f8ce214fe
sarika12s <sarikachilukamari@gmail.com>
2026-08-14 11:15:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Min Diff Pair/min-diff-pair.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minDiff(int[] arr) {
+        Arrays.sort(arr);
+        int minDiff =Integer.MAX_VALUE;
+        for(int i=1; i<=arr.length-1; i++){
+           minDiff =Math.min(minDiff,arr[i]-arr[i-1]);
+        }
+        return minDiff;
+        
+    }
+}
